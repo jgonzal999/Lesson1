c306a7a73d8ed5c0408597dfa971f10e87be9204
Javier <jgonzal99@gmail.com>
2023-07-31 21:24:40 +0000

    Calculator2

--- Lesson1/src/calculator/menu/Menus.java
+++ Lesson1/src/calculator/menu/Menus.java
@@ -5,13 +5,16 @@ public class Menus {
 		System.out.println("Welcome to my Calculator");
 	}
 	public void mainmenu() {
-		System.out.println("Please select the operation:\n 1.Addition\n 2.Substraction\n 3.Multiplication\n 4.Division\n 5.EXIT");
+		System.out.println("\nPlease select the operation:\n\n 1.Addition       (a+b)\n 2.Substraction   (a-b)\n 3.Multiplication (a*b)\n 4.Division       (a/b)\n\n 5.QUIT");
 	}
 	public void insertnumber(char num) {
 		System.out.println("Please insert number "+num+": ");
 	}
+	public void result(String oper, double res) {
+		System.out.printf("The Result of "+oper+" : %.2f\n",res);
+	}
 	public void cont() {
-		System.out.println("Continue? (y/n): ");
+		System.out.println("Do you really want to quit the app? (y/n): ");
 	}
 	public void bye() {
 		System.out.println("Bye, thank you for using our products!");
